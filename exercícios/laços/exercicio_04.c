bef6abd8666ebbb16677a30ca9ec3b9c852cce15
henryzord <henry.cagnini@gmail.com>
2022-11-25 13:45:53 -0300

    solved fourth loop exercise

--- exercícios/laços/exercicio_04.c
+++ exercícios/laços/exercicio_04.c
@@ -1,3 +1,15 @@
-exercicio_01.c//
-// Created by henry on 25/11/22.
-//
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    if(n < 1 || n > 26) {
+        printf("n deve estar entre 1 e 26!\n");
+        return 1;
+    }
+    printf("o %d numero do alfabeto e %c\n", n, 97 + (n - 1));
+
+    return 0;
+}
