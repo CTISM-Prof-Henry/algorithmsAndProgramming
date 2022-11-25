8a7e862594b97642eed68f4d2ece3b5896baf6f8
henryzord <henry.cagnini@gmail.com>
2022-11-25 13:41:29 -0300

    solved second exercise

--- exercícios/laços/exercicio_02.c
+++ exercícios/laços/exercicio_02.c
@@ -1,3 +1,23 @@
-exercicio_01.c//
-// Created by henry on 25/11/22.
-//
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    if(n < 0) {
+        printf("Este algoritmo ainda nao resolve fatoriais negativos!\n");
+        return 1;
+    }
+    if(n == 0) {
+        printf("0! = %d\n", 1);
+    } else {
+        int prod = 1;
+        for(int i = 1; i <= n; i++) {
+            prod = prod * i;
+        }
+        printf("%d! = %d\n", n, prod);
+    }
+
+    return 0;
+}
