cf90c473f9000b99f7f946bc38688eefc07ab3a5
henryzord <henry.cagnini@gmail.com>
2022-11-25 13:48:39 -0300

    solved fifth loop exercise

--- exercícios/laços/exercicio_05.c
+++ exercícios/laços/exercicio_05.c
@@ -1,3 +1,24 @@
-exercicio_01.c//
-// Created by henry on 25/11/22.
-//
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    if(n < 1) {
+        printf("n deve ser maior que 1!\n");
+        return 1;
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            printf("%d ", 1);
+        }
+        for(int j = i + 1; j < n; j++) {
+            printf("%d ", 0);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    return 0;
+}
